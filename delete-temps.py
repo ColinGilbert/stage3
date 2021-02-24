f63158056d44a1cf539900ac59de3735bc357d8f
Colin Gilbert <colingilbert86@gmail.com>
2021-02-24 01:10:23 +0000

    Fixups

--- delete-temps.py
+++ delete-temps.py
@@ -10,7 +10,9 @@ os.system('git checkout master')
 
 for t in tags:
     os.system('git checkout ' + t)
-    os.system('rm Dockerfile *~ .*.sw*')
+    os.system('rm Dockerfile')
+    os.system('rm *~')
+    os.system('rm .*.sw*')
     os.system('rm -rf __pycache__')
     os.system('git add . && git commit -m "Cleanup"')
 
