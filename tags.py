7368b1e639e35e46a862cd39a98e27d11eca5846
Colin Gilbert <colingilbert86@gmail.com>
2021-02-24 03:30:41 +0000

    Cleanup

--- tags.py
+++ tags.py
@@ -1,2 +1,2 @@
 def get_tags():
-    return ['latest', 'amd64-hardened-nomultilib', 'amd64-nomultilib', 'amd64-musl-hardened', 'amd64-musli-vanilla', 'amd64-uclibc-hardened']
+    return ['latest', 'amd64-hardened-nomultilib', 'amd64-nomultilib', 'amd64-musl-hardened', 'amd64-musl-vanilla', 'amd64-uclibc-hardened']
