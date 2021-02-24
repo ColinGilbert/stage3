43cc79a6a526f787a409254c677d3a31fd370a9f
Colin Gilbert <colingilbert86@gmail.com>
2021-02-24 04:25:59 +0000

    latest

--- checkout-branches.py
+++ checkout-branches.py
@@ -18,11 +18,9 @@ for t in tags:
     os.system('git checkout -b ' + t)
     head_line = first_line + ":" + t + '\n'
     lines[0] = head_line
-    os.system('rm Dockerfile')
     with open('Dockerfile', 'w') as outfile:
         outfile.writelines(lines)
-    os.system('rm -rf __pycache__')
-    os.system('git add .')
-    os.system('git commit  -m "' + t + '"')
+    os.system('rm -rf __pycache__ & rm Dockerfile.in')
+    os.system('git add . && git commit  -m "' + t + '"')
 
 os.system('git checkout master && rm Dockerfile')
