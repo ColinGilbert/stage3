81277407e1160e8a5d4e371c74549982e7c98822
Colin Gilbert <colingilbert86@gmail.com>
2021-02-24 01:06:32 +0000

    Cleanup

---
+++ delete-temps.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import os
+
+from tags import get_tags
+
+tags = get_tags()
+
+os.system('git checkout master')
+
+for t in tags:
+    os.system('git checkout ' + t)
+    os.system('rm -Dockerfile .*.sw*')
+    os.system('rm -rf __pycache__')
+    os.system('git add . && git commit -m "Cleanup"')
+
+os.system('git checkout master')
